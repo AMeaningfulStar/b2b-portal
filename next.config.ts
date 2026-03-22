d2ff8a2ec6d68736b08094115a91c0899b6d212b
A Meaningful star <ghrlfehd123@naver.com>
2026-03-22 13:54:27 +0900

    chore: 외부 이미지 사용 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: process.cwd(),
+  },
   images: {
-    domains: ['placehold.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
